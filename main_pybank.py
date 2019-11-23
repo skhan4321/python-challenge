2bcbd0ad3b2538e845642bf73fa966840eb2e961
Sana Khan <sana.khan@sailpoint.com>
2019-11-23 16:27:55 -0600

    add info about profit

--- main_pybank.py
+++ main_pybank.py
@@ -1,3 +1,16 @@
 import csv 
 
 import os 
+
+csvpath = os.path.join("PyBank","budget_data.csv")
+#variables
+total_months = 0
+total_profit = 0 
+
+# open the file and run code
+with open(csvpath,'r') as budget_analysis:
+    csvreader = csv.DictReader(budget_analysis)
+    for row in csvreader:
+        total_months = total_months + 1
+        total_profit = total_profit + int(row["Profit/Losses"])
+      
